10f8c79cae89e24f53df4a71623e66105590f396
r-neal-kelly <r.neal.kelly@gmail.com>
2021-05-17 22:56:38 -0400

    in addtion to disabling saving, we've also disabled actor fetching altogether

--- Source/Plugins/doticu_npcl/src/mcm_references_loaded.cpp
+++ Source/Plugins/doticu_npcl/src/mcm_references_loaded.cpp
@@ -51,7 +51,8 @@ namespace doticu_npcl { namespace MCM {
             read.reserve(2048);
             read.clear();
             //Actor_t::All(reinterpret_cast<Vector_t<some<Item_t>>&>(read));
-            Actor_t::All_Actors_In_Cells(reinterpret_cast<Vector_t<some<Item_t>>&>(read));
+            //Actor_t::All_Actors_In_Cells(reinterpret_cast<Vector_t<some<Item_t>>&>(read));
+            _MESSAGE("temporarily disabled actor fetching.");
 
             write.reserve(2048);
             write.clear();
